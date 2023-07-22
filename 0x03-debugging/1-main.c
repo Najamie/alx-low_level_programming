617b2e51949cdcff9d7b9f73be77795c1a374107
Najamie <abdimalikhassan606@gmail.com>
2023-07-21 18:00:00 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+/**
+ * main-causenan infinate loop
+ * return 0
+ */
 
+int main(void)
+{
+	int i;
+
+	printf("infinate loop incoming :(/n");
+
+	i=0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*      putcher(i);*/
+
+	/*}*/
+
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+
+}
