fa4e0ffcf81fc01780516a1fd920271dbce176d8
Najamie <abdimalikhassan606@gmail.com>
2023-08-01 17:21:34 -0700

    https://intranet.alxswe.com/projects/218#quiz-completed

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,29 +1,19 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_chessboard - prints the chess board
- *
- * @a: input pointer
- *
- * Return: return nothing
-*/
-
+ * print_chessboard - Prints the chessboard.
+ * @a: 2D array of characters representing the chessboard.
+ */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-
-	i = 0;
-	j = 0;
-	while (i < 64)
-	{
-		if (i % 8 == 0 && i != 0)
-		{
-			j = i;
-			_putchar('\n');
-		}
-
-		_putchar(a[i / 8][i - j]);
-		i++;
-	}
-	_putchar('\n');
+    int i, j;
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            putchar(a[i][j]);
+        }
+        putchar('\n');
+    }
 }
+
