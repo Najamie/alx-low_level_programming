7ed5eec6dee591750c87c338b3f1613f9d051810
Najamie <abdimalikhassan606@gmail.com>
2023-10-19 13:45:36 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,10 +18,12 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-/* Prototype for the print_list function */
-size_t print_list(const list_t *h);
 
-/* Prototype for the _putchar function */
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 int _putchar(char c);
 
 #endif
