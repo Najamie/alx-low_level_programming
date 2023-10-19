02ec2b6b80c1e9622bf65e3f24adf740c718e497
Najamie <abdimalikhassan606@gmail.com>
2023-10-19 02:19:58 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,18 +1,49 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-/**
- * free_grid - frees the assigned memory of the matrix
- * @grid: the matrix
- * @height: number of rows.
- *
- * Return: pointer to the matrix.
- */
-void free_grid(int **grid, int height)
-{
-	int i;
 
-	for (i = 0; i < height; i++)
-		free(*(grid + i));
-	free(grid);
+/**
+* argstostr - concatenates all the arguments of your program.
+* @ac: arguments count
+* @av: arguments vector
+*
+* Return: a pointer to a new string, or NULL if it fails
+*/
+char *argstostr(int ac, char **av)
+{
+  char *str, *s;
+  int i, j, k, len = 0;
+
+  if (ac == 0 || av == NULL)
+     return (NULL);
+
+  for (i = 0; i < ac; i++)
+  {
+     s = av[i];
+     j = 0;
+
+     while (s[j++])
+        len++;
+     len++;
+  }
+
+  str = (char *)malloc(sizeof(char) * (len + 1));
+  if (str == NULL)
+     return (NULL);
+
+  for (i = 0, j = 0; i < ac && j < len; i++)
+  {
+     s = av[i];
+     k = 0;
+
+     while (s[k])
+     {
+        str[j] = s[k];
+        k++;
+        j++;
+     }
+     str[j++] = '\n';
+  }
+  str[j] = '\0';
+
+  return (str);
 }
